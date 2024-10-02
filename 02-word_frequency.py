3748de1ff5a1998e11f743c9956740872bb79e64
RayanHSaeed <181386496+RayanHSaeed@users.noreply.github.com>
2024-10-02 05:02:51 +0000

    solving part02

--- 02-word_frequency.py
+++ 02-word_frequency.py
@@ -20,14 +20,14 @@ Hints:
 import sys
 
 def word_frequency(text):
-    frequencies = {'name': 'Alice', 
-                   'location':'Wonderland',
-                   'direction': 'down', 
-                   'animal': 'cat', 
-                   'object': 'book',
-                   'action': 'climb', 
-                   'pronoun': 'she', 
-                   'color': 'golden'} # Dictionary to store word frequencies 
+    frequencies = {}                  # Dictionary to store word frequencies 
+   
+    words = text.lower().split()      #convert text to lowercase and splits into words
+    for word in words:                #iterate thru words in the list
+        if word in frequencies: 
+            frequencies[word] += 1
+        else: 
+            frequencies[word] = 1     #if word is in dictionary, add one to count, otherwise add the word to dictionary with a count of 1
     return frequencies
 
 # Scaffold for opening a file and running word_frequency() on the contents
@@ -53,3 +53,6 @@ if __name__ == "__main__":
     
     print(f"Word frequencies for '{filename}':")
     print(frequencies)
+
+
+#use python 02-word_frequency.py alice_in_wonderland.txt to run
